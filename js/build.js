3e9687feaa9eca38f916ce9b1e2d8867799ef4e9
Serhii Krupnyk <84472843+skrupnyk@users.noreply.github.com>
2022-06-07 09:35:45 +0300

    OD-838 [Fix] Fixed initialization for widget (#51)
    * add translate function
    
    * Added wait promise on init
    
    * fix
    
    * update
    
    * refactor
    
    * debug
    
    * debug
    
    * refactor
    
    * fix

--- js/build.js
+++ js/build.js
@@ -1,5 +1,6 @@
 Fliplet.Widget.instance('list', function(data) {
   var $container = $(this);
+  var _this = this;
 
   $container.find('.linked[data-list-item-id]').click(function(event) {
     event.preventDefault();
@@ -22,12 +23,14 @@ Fliplet.Widget.instance('list', function(data) {
 
   var swipeToSaveLabel = data.swipeToSaveLabel || T('widgets.list.noImages.defaultListName');
 
-  $container.translate({ swipeToSaveLabel: swipeToSaveLabel });
+  Fliplet().then(function() {
+    $container.translate({ swipeToSaveLabel: swipeToSaveLabel });
 
-  if (data.swipeToSave) {
-    window.ui = window.ui || {};
-    window.ui['swipeSavedList' + $container.attr('data-list-uuid')] = new SwipeSaveList(this, {
-      savedListLabel: swipeToSaveLabel
-    });
-  }
+    if (data.swipeToSave) {
+      window.ui = window.ui || {};
+      window.ui['swipeSavedList' + $container.attr('data-list-uuid')] = new SwipeSaveList(_this, {
+        savedListLabel: swipeToSaveLabel
+      });
+    }
+  });
 });
