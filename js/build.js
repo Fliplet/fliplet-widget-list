860d10d42865c3fb9ffd3f878396e4262c3ff037
Dawid Witulski <dwitulski@fliplet.com>
2024-09-17 00:57:12 +0200

    fix(PS-223): added keyboard support for links

--- js/build.js
+++ js/build.js
@@ -2,22 +2,31 @@ Fliplet.Widget.instance('list-1-0-1', function(data) {
   var $container = $(this);
   var _this = this;
 
-  $container.find('.linked[data-list-item-id]').click(function(event) {
+  const handleItemClick = (event) => {
     event.preventDefault();
 
-    var $listItem = $(this);
+    const listItem = event.currentTarget;
 
-    if ($listItem.parents('.list-swipe.swiping').length) {
+    if (listItem.closest('.list-swipe.swiping')) {
       return;
     }
 
-    var itemData = _.find(data.items, {
-      id: $listItem.attr('data-list-item-id')
-    });
+    const itemId = listItem.getAttribute('data-list-item-id');
+    const itemData = data.items.find(item => item.id === itemId);
 
-    if (!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+    if (itemData?.linkAction) {
       Fliplet.Navigate.to(itemData.linkAction);
     }
+  };
+
+  const linkedItems = $container[0].querySelectorAll('.linked[data-list-item-id]');
+  linkedItems.forEach(item => {
+    item.addEventListener('click', handleItemClick);
+    item.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' || event.key === ' ') { 
+        handleItemClick(event);
+      }
+    });
   });
 
   Fliplet().then(function() {
